1594f3d78ab8151ad1e93c65aba035d3e28b0d3e
SAIFRAHMAN07 <itzsaif292@gmail.com>
2026-08-11 00:01:19 +0530

    Time: 1 ms (97.07%), Space: 47.2 MB (60.65%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null) {
+
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } 
+            else {
+                prev = prev.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
